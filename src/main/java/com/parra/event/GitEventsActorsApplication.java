71f38c7185735415af047e36cc6fe61cdb26ed8d
guaica63 <parraflorensa@hotmail.com>
2019-06-26 09:46:14 +0200

    Actualizacion sobre servicios

--- src/main/java/com/parra/event/GitEventsActorsApplication.java
+++ src/main/java/com/parra/event/GitEventsActorsApplication.java
@@ -2,6 +2,9 @@ package com.parra.event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class GitEventsActorsApplication {
